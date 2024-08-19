094bd9a36e8315da920128cf9bd40b9bfc11648a
IliaLataria01 <latariailia6@gmail.com>
2024-08-19 15:16:46 +0400

    initial commitssxx

---
+++ src/main/java/com/example/demo/AAA.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class AAA {
+
+    int x = 10;
+    public void draw() {
+
+    }
+}
--- src/main/java/com/example/demo/BB.java
+++ src/main/java/com/example/demo/BB.java
@@ -3,4 +3,7 @@ package com.example.demo;
 public class BB {
 
     public void draw() {}
+
+    public void draw1() {}
+
 }
